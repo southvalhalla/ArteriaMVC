40f00efa3bbb6c3c3d3412dbfebe805e06d3e914
southvalhalla <ah.alejo25@gmail.com>
2023-03-24 17:11:35 -0500

    fix charset

--- app/config/dataBase.php
+++ app/config/dataBase.php
@@ -4,17 +4,19 @@
         private $dataBase;
         private $username;
         private $password;
+        private $charset;
 
         public function __construct () {
-        $this->host = constant('HOST');
+        $this->host     = constant('HOST');
         $this->dataBase = constant('DATABASE');
         $this->username = constant('USER');
         $this->password = constant('PASSWORD');
+        $this->charset  = constant('CHARSET');
         }
 
         public function connect () {
             try{
-                $conn = new PDO ("mysql:host=$this->host;dbname=$this->dataBase",$this->username, $this->password);
+                $conn = new PDO ("mysql:host=$this->host;dbname=$this->dataBase;charset=$this->charset",$this->username, $this->password);
                 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 return $conn;
             } catch(PDOException $e){
